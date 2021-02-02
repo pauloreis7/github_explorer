f4e29ee43780029c1523e6cf2bafc09b8287d206
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-02-02 18:59:54 -0300

    Construindo estrutura e estilização das telas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/63323224?s=460&u=2daadb593c5a951e43762aa302f5c42a60d6f6d7&v=4"
+            alt="Paulo Reis"
+          />
+
+          <div>
+            <strong>Paulo Reis7</strong>
+            <p>Aplicação de vendas de produtos por usuários</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/63323224?s=460&u=2daadb593c5a951e43762aa302f5c42a60d6f6d7&v=4"
+            alt="Paulo Reis"
+          />
+
+          <div>
+            <strong>Paulo Reis7</strong>
+            <p>Aplicação de vendas de produtos por usuários</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/63323224?s=460&u=2daadb593c5a951e43762aa302f5c42a60d6f6d7&v=4"
+            alt="Paulo Reis"
+          />
+
+          <div>
+            <strong>Paulo Reis7</strong>
+            <p>Aplicação de vendas de produtos por usuários</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
